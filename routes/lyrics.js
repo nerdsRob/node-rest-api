66bd98627b3d4c8b6b7aa99ccf29eb8af3109fcf
Rob Siwek <rob@robsiwek.com>
2017-12-24 12:48:57 +0100

    Addes redis for caching

--- routes/lyrics.js
+++ routes/lyrics.js
@@ -3,27 +3,58 @@ require('dotenv').config();
 const express = require('express');
 const router = express.Router();
 const Lyricist = require('lyricist');
+const redis = require('redis');
+const client = redis.createClient();
 
 lyricist = new Lyricist(process.env.GENIUS_API_TOKEN);
 
-router.get('/search/:term', async (request, response, next) => {
+const fetchLyrics = async (req, res) => {
   try {
-    const searchTerm = request.params.term;
-    const results = await lyricist.search(searchTerm);
-    response.json(results);
-  } catch (e) {
-    next(e)
-  }
-})
-
-router.get('/:songId', async (request, response, next) => {
-  try {
-    const songId = request.params.songId;
+    const songId = req.params.songId;
     const song = await lyricist.song(songId, { fetchLyrics: true });
-    response.json(song);
+    client.setex(songId, 3600, JSON.stringify(song));
+    res.json(song);
   } catch (e) {
     next(e)
   }
-})
+};
+
+const readSongFromCache = (req, res) => {
+  const songId = req.params.songId;
+  client.get(songId, (err, result) => {
+    if (result) {
+      res.send(result);
+      console.log("Cache hit {song_id}: " + songId);
+    } else {
+      fetchLyrics(req, res);
+    }
+  });
+}
+
+const search = async (req, res) => {
+  try {
+    const searchTerm = req.params.term;
+    const results = await lyricist.search(searchTerm);
+    client.setex(searchTerm, 3600, JSON.stringify(results));
+    res.json(results);
+  } catch (e) {
+    next(e)
+  }
+}
+
+const readSearchResultsFromCache = (req, res) => {
+  const searchTerm = req.params.term;
+  client.get(searchTerm, (err, result) => {
+    if (result) {
+      res.send(result);
+      console.log("Cache hit {search_term}: " + searchTerm);
+    } else {
+      search(req, res);
+    }
+  });
+}
+
+router.get('/:songId', readSongFromCache);
+router.get('/search/:term', readSearchResultsFromCache);
 
 module.exports = router;
